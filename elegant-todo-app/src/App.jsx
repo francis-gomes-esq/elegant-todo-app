46b971a20213f0cd95989d0d52fe4a3c8a7892bd
francis-gomes-esq <figomesokoye@protonmail.com>
2024-03-23 22:14:50 +0000

    add toggle and delete funtions

--- elegant-todo-app/src/App.jsx
+++ elegant-todo-app/src/App.jsx
@@ -16,6 +16,24 @@ export default function App(){
 		setNewItem('')
 	}	
 
+	function toggleTodo(id, completed){
+		setTodos(currentTodos =>{
+			return currentTodos.map(todo =>{
+				if(todo.id === id){
+					return {...todo, completed}
+				}
+				return todo
+			})
+		})
+	}
+
+
+	function deleteTodo(id){
+		setTodos(currentTodos => { 
+			return currentTodos.filter(todo => todo.id !== id)
+		})
+	}
+
   return (
     <>
     <form  onSubmit={handleSubmit} className="new-item-form">
@@ -33,10 +51,17 @@ export default function App(){
 				return  (
 					<li key={todo.id}>
 				<label>
-        	<input type="checkbox" checked={todo.completed}/>
+        	<input type="checkbox" checked={todo.completed}
+					onChange={e => toggleTodo(todo.id, e.target.checked)}
+					/>					
+		
         	{todo.title}
 				</label>
-        <button className="btn btn-danger">Delete</button>
+        <button 
+				onClick={()=> deleteTodo(todo.id)} 
+				className="btn btn-danger">
+					Delete
+				</button>
       </li>
 				)
 			})}			
