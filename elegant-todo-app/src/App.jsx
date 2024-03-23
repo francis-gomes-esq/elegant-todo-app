1dcf9ed3ec2ee81745544bdf5efe780d78cfd952
francis-gomes-esq <figomesokoye@protonmail.com>
2024-03-23 21:53:39 +0000

    place code-block inside fragment,  And add logic to render the addbutton include empty array to clear items entered into the input fields after clicking on the add button

--- elegant-todo-app/src/App.jsx
+++ elegant-todo-app/src/App.jsx
@@ -3,10 +3,19 @@ import "./styles.css"
 
 export default function App(){
 	const [newItem, setNewItem] = useState('')
+	const [todos, setTodos] = useState([])
 
 	function handleSubmit(e){
 		e.preventDefault()
-	}
+		
+		setTodos((currentTodos)=> {
+					return [
+				...currentTodos, {id: crypto.randomUUID(), title:newItem, completed: false}, 
+			]
+		})	
+		setNewItem('')
+	}	
+
   return (
     <>
     <form  onSubmit={handleSubmit} className="new-item-form">
@@ -20,23 +29,18 @@ export default function App(){
     </form>
     <h1 className="header">Todo List</h1>
     <ul className="list">
-      <li>
+			{todos.map(todo => {
+				return  (
+					<li key={todo.id}>
 				<label>
-        <input type="checkbox" />
-        Item 1
+        	<input type="checkbox" checked={todo.completed}/>
+        	{todo.title}
 				</label>
         <button className="btn btn-danger">Delete</button>
       </li>
-			<li>
-				<label>
-        <input type="checkbox" />
-        Item 2
-				</label>
-        <button className="btn btn-danger">Delete</button>
-      </li>
-			
+				)
+			})}			
     </ul>
     </>
   )
-
 }
