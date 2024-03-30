5fdee270accb94bb5ffcf7b14f19de689b45a896
francis-gomes-esq <figomesokoye@protonmail.com>
2024-03-30 02:07:55 +0000

    change cta copy

--- elegant-todo-app/src/NewTodoForm.jsx
+++ elegant-todo-app/src/NewTodoForm.jsx
@@ -16,12 +16,12 @@ export function NewTodoForm({onSubmit}){
   return(
     <form  onSubmit={handleSubmit} className="new-item-form">
     <div className="form-row">
-      <label htmlFor="item">New Item</label>
+      <label htmlFor="item">Add Item</label>
       <input value={newItem} onChange={e => setNewItem(e.target.value)} 
       type="text" 
       id="item"/>
     </div>
-    <button className="btn">Add</button>
+    <button className="btn">ADD</button>
   </form>
   )
 }
